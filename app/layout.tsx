0f6386db6009304b535cec7bd1791116c807d421
kaikunnnn <kyasya00@gmail.com>
2024-10-05 16:30:26 +0900

    Install Shadcn-Ui

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,7 +37,8 @@ export default function RootLayout({
               <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
                 <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
                   <div className="flex gap-5 items-center font-semibold">
-                    <Link href={"/"}>Next.js Supabase Starter</Link>
+                    <Link href={"/"}>Next.js Supabase aaaaa</Link>
+                    
                     <div className="flex items-center gap-2">
                       <DeployButton />
                     </div>
@@ -48,6 +49,8 @@ export default function RootLayout({
               <div className="flex flex-col gap-20 max-w-5xl p-5">
                 {children}
               </div>
+              
+             
 
               <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16">
                 <p>
